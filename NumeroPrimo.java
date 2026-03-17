61cc1f500a41980e6344fc76c17bafc8bd380364
Mauricio Cecilio Martinez Cornejo <mauriciowaso@gmail.com>
2026-03-17 12:35:27 -0400

    inicio del Laboratorio 4

---
+++ NumeroPrimo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        boolean esPrimo = true;
+
+        if (numero <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i < numero; i++) {
+                if (numero % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (esPrimo) {
+            System.out.println("El número es PRIMO");
+        } else {
+            System.out.println("El número NO es PRIMO");
+        }
+
+        scanner.close();
+    }
+}
